05c23a28e354f304eed81740153d06463a703513
sadiyarehman <sadiya786rehman@gmail.com>
2026-05-02 15:11:09 +0530

    all java programs

---
+++ Citizen.java
@@ -0,0 +1,30 @@
+
+
+public final class Citizen {
+	String name;
+	final static String countryName="India";
+	
+	public  Citizen(String name) {
+		this.name = name;
+	}
+
+	void m1() {
+		System.out.println("In method 1");
+	}
+	
+	final void m2() {
+		System.out.println("In method 2");
+	}
+
+	@Override
+	public String toString() {
+		return "Citizen [name=" + name  +"country ="+countryName +"]";
+	}
+    public static void main(String[] args) {
+    Citizen c = new Citizen("Alice");
+    System.out.println(c);
+}
+
+	
+	
+}
